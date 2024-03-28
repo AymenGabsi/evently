9373a7f4f6346b66d3c8b83a56611afc08d19ea8
AymenGabsi <aymengabsi20@gmail.com>
2024-03-28 22:21:41 +0100

    [Evently] Making progress in the Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Eventlylogo"
+          />
+        </Link>
 
-export default Header
+        <div className="flex w-32 justify-end gap-3">
+
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
